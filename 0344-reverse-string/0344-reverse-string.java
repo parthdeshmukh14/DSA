0f408dce08c31c7fa8b5ab90b277f0a961b3c996
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-09 23:21:04 +0530

    Time: 0 ms (100%), Space: 48.2 MB (55.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,7 +2,7 @@ class Solution {
     public void reverseString(char[] s) {
         int left=0;
         int right=s.length-1;
-        for(int i=0;i<s.length;i++){
+        
             while(left<=right){
                 char temp=s[left];
                 s[left]=s[right];
@@ -10,6 +10,6 @@ class Solution {
                 left++;
                 right--;
             }
-        }
+        
     }
 }
