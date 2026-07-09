efc37d4b3b205b5d833239c949f81b2ff3791d51
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-09 22:21:36 +0530

    Time: 4 ms (82.34%), Space: 44.5 MB (67.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,26 +6,41 @@ class Solution {
         if(inp.length() != op.length()){
             return false;
         }
-        HashMap<Character,Integer> res=new HashMap<>();
+        //better solution 
+        // HashMap<Character,Integer> res=new HashMap<>();
        
-        for(int i=0;i<inp.length();i++){
-            char ch=inp.charAt(i);
-            res.put(ch,res.getOrDefault(ch,0)+1);
-        }
+        // for(int i=0;i<inp.length();i++){
+        //     char ch=inp.charAt(i);
+        //     res.put(ch,res.getOrDefault(ch,0)+1);
+        // }
         
 
+        // for(int i=0;i<op.length();i++){
+        //     char ch=op.charAt(i);
+        //     if(!res.containsKey(ch)){
+        //         return false;
+        //     }
+        //     else{
+        //       res.put(ch,res.get(ch)-1);  
+        //     }
+        //     if(res.get(ch)==0){
+        //         res.remove(ch);
+        //     }
+        // }
+        // return res.isEmpty();
+        //optimal solution
+        int[] freq=new int[26];
+        for(int i=0;i<inp.length();i++){
+            freq[inp.charAt(i)-'a']++;
+        }
         for(int i=0;i<op.length();i++){
-            char ch=op.charAt(i);
-            if(!res.containsKey(ch)){
-                return false;
-            }
-            else{
-              res.put(ch,res.get(ch)-1);  
-            }
-            if(res.get(ch)==0){
-                res.remove(ch);
+            freq[op.charAt(i)-'a']--;
+        }
+        for(int cnt:freq){
+            if(cnt !=0){
+                return false; 
             }
         }
-        return res.isEmpty();
+        return true;
     }
 }
