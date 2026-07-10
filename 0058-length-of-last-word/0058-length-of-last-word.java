bb210561bc678dc4b69ebf6df9fd6b7f5f1e2418
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-11 01:00:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.9%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(length==0 && ch==' '){
+                continue;
+
+            }
+            else if(ch!=' '){
+                length++;
+            }
+            else{
+                break;
+            }
+        }
+        return length;
+    }
+}
