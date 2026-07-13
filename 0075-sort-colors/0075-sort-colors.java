23b40a8114e4ac09d95425636757d3cb38f98a7a
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-13 18:49:39 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,40 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        // int left=0;
-        // int right=n-1;
-        // while(left<right){
-        //     if(nums[left]>nums[right]){
-        //         int temp=nums[left];
-        //         nums[left]=nums[right];
-        //         nums[right]=temp;
-        //         left++;
-        //         right--;
-        //     }
-            
-        //     else{
-        //         left++;
+        //brute solution
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]>nums[j]){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
         //     }
         // }
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==2){
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                
+                high--;
+            }
+            
+            else if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            }
+            else{
+                mid++;
             }
         }
+        
 
     }
 }
