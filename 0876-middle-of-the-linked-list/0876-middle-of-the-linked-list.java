6600d1906d8b82983cbfec63ed64f7177b8903cc
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-25 17:44:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.46%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -11,22 +11,31 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         //if we were creating linklist we would have use the list.size();
-        int nodecount=0;
-        ListNode prev=null;
-        ListNode current=head;
-        while(current != null){
+        // int nodecount=0;
+        // ListNode prev=null;
+        // ListNode current=head;
+        // while(current != null){
            
-            nodecount++;
-            current = current.next;
-        }
-        int mid=nodecount/2;
-        current=head;
+        //     nodecount++;
+        //     current = current.next;
+        // }
+        // int mid=nodecount/2;
+        // current=head;
 
         
-        while(mid>0){
-            current=current.next;
-            mid--;
+        // while(mid>0){
+        //     current=current.next;
+        //     mid--;
+        // }
+        // return current;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return current;
+
+        return slow;
     }
 }
