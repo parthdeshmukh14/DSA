fc2827771342d093267fb497c24cd6ece747a178
Parth Deshmukh <parthdeshmukh5909@gmail.com>
2026-07-29 07:15:34 +0530

    Time: 0 ms (100%), Space: 42 MB (52.6%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,30 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
+        // if(n==0){
+        //     return 0;
+        // }
+        // else if(n==1){
+        //     return 1;
+        // }
+        // else{
+        //     return fib(n-1)+fib(n-2);
+        // }
+        int left=0;
+        int right=1;
+        int op=0;
+             if(n==0){
+            return left;
         }
-        else if(n==1){
-            return 1;
+            else if(n==1){
+            return right;
         }
-        else{
-            return fib(n-1)+fib(n-2);
+        for(int i=2;i<=n;i++){
+       
+            op=left+right;
+            left=right;
+            right=op;
         }
+        return op;
+
     }
 }
